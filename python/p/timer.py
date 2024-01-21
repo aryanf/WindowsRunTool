130dbc22dee2519e2c8e3cf263b0f60b9403bddb
Aryan <aryan_firouzyan@hotmail.com>
2024-01-21 02:56:06 +0200

    Add timer.py and hourglass

---
+++ python/p/timer.py
@@ -0,0 +1,18 @@
+from message import (MainCommandMessage, SubCommandMessage, get_open_source_app_dir)
+import subprocess
+import os
+
+
+app_path = os.path.join(get_open_source_app_dir(), 'HourGlass', 'Hourglass.exe')
+
+def main(message: MainCommandMessage):
+    '''
+Stopwatch 
+{switch_1}: Unit time like d, day, days, h, hour, hours, m, minute, minutes, s, sec, second, seconds. Default is hour
+{num}: Number of unit to count. Default is 1
+'''
+    # check https://alternativeto.net/software/hourglass-timer/about/ to modify command parameters
+    units = ['d', 'day', 'days', 'h', 'hour', 'hours', 'm', 'minute', 'minutes', 's', 'sec', 'second', 'seconds']
+    unit = message.switch_1 if message.switch_1 in units else 'hour'
+    param = f'{message.num} {unit}'
+    subprocess.Popen([app_path, '--loop-sound', 'on', param])
