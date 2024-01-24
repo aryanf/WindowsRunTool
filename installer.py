c0cc737696fe1cecdb7c5516a1fb57be535c4002
Aryan <aryan_firouzyan@hotmail.com>
2024-01-25 00:40:39 +0200

    Add env var by install.py

--- installer.py
+++ installer.py
@@ -1,21 +1,29 @@
 import os
 import win32com.client
+from python.env_var_utils import prepend_env
 
-key_commands = ['p']
+key_commands = ['p', 'i', 'l']
 
-current_directory = os.getcwd()
+current_dir = os.getcwd()
+
+# This is going to add current directory to path env var
+# If you face a problem here, comment this code and add it manually
+prepend_env('Path', [
+    current_dir
+    ])
+
 for key_command in key_commands:
-    bat_file_path = os.path.join(current_directory, f'{key_command}.bat')
-    shortcut_file_path = os.path.join(current_directory, f'{key_command}.lnk')
-    python_script_path = os.path.join(current_directory, 'python', 'run.py')
-    directory_path = os.path.join(current_directory, 'python', key_command)
+    bat_file_path = os.path.join(current_dir, f'{key_command}.bat')
+    shortcut_file_path = os.path.join(current_dir, f'{key_command}.lnk')
+    python_script_path = os.path.join(current_dir, 'python', 'run.py')
+    directory_path = os.path.join(current_dir, 'python', key_command)
 
     # Check if bat and shortcut already exist
     if os.path.exists(bat_file_path) and os.path.exists(shortcut_file_path):
         print('bat and shortcut already exist. Skipping creation.')
     else:
         bat_content = f'''@echo off
-Set "base={current_directory}"
+Set "base={current_dir}"
 if ["%~1"]==[""] (
 	python "%base%/python/run.py" {key_command}
 ) else if ["%~2"]==[""] (
@@ -43,7 +51,7 @@ if ["%~1"]==[""] (
         shortcut.Targetpath = bat_file_path
         shortcut.save()
 
-        print(f'Successfully created bat and shortcut in {current_directory}.')
+        print(f'Successfully created bat and shortcut in {current_dir}.')
 
     if not os.path.exists(directory_path):
         # Create the directory
