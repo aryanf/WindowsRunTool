af9bc0972080320565c649a9ff94f0294ba21f7a
aryan.firouzian <aryan.firouzian@hansencx.com>
2024-07-06 15:43:59 +0300

    Let 'd list' to remove and create virtual desktops

--- python/d/list.py
+++ python/d/list.py
@@ -10,14 +10,20 @@ import curses_terminal
 def _get_desktop(current_desktop: VirtualDesktop):
     desktops = get_virtual_desktops()
     desktop_names = [d.name for d in get_virtual_desktops()]
+    desktop_names.append('--- create new desktop ---')
     i, cmd = curses_terminal.show(options=desktop_names, enumerating=True, zero_indexed=False, info=f'Current Desktop: {current_desktop.name}')
     if cmd == 'exit' or cmd == 'q' or cmd == 'quit' or cmd == 'e':
         exit()
+    if cmd == '--- create new desktop ---':
+        name = input('Enter new desktop name: ')
+        new_desktop = VirtualDesktop.create()
+        new_desktop.rename(name)
+        _get_desktop(new_desktop)
     selected_desktop = desktops[i]
     _get_apps(selected_desktop)
 
 def _get_apps(selected_desktop: VirtualDesktop):
-    app_names = ['go', '..']
+    app_names = ['--- open ---', '--- remove ---', '..']
     apps = selected_desktop.apps_by_z_order()
     for app in apps:
         try:
@@ -29,9 +35,20 @@ def _get_apps(selected_desktop: VirtualDesktop):
     i, cmd = curses_terminal.show(options=app_names, enumerating=True, zero_indexed=False, info=f'Selected Desktop: {selected_desktop.name}')
     if cmd == 'exit' or cmd == 'q' or cmd == 'quit' or cmd == 'e':
         exit()
-    if i == 0:
+    elif cmd == '--- open ---':
         VirtualDesktop.go(selected_desktop)
         exit()
+    elif cmd == '--- remove ---':
+        if len(apps) > 0:
+            response = input('There are open apps in this desktop. Are you sure you want to remove this desktop?')
+            if response == 'y' or response == 'yes' or response == '1' or response == 'true' or response == 't' or response == 'ok' or response == 'sure' or response == 'yup' or response == 'yeah' or response == 'yea' or response == 'yep' or response == 'ok':
+                selected_desktop.remove()
+                _get_desktop(selected_desktop)
+            else:
+                _get_apps(selected_desktop)
+        else:
+            selected_desktop.remove()
+            _get_desktop(selected_desktop)
     elif i == 1:
         _get_desktop(selected_desktop)
     else:
@@ -39,7 +56,7 @@ def _get_apps(selected_desktop: VirtualDesktop):
         desktop_names = [d.name for d in get_virtual_desktops()]
         j, cmd = curses_terminal.show(options=desktop_names, enumerating=True, zero_indexed=False, info=f'Move {app_names[i]} to other desktop')
         target_desktop = desktops[j]
-        selected_window = apps[i-2]
+        selected_window = apps[i-3]
         selected_window.move(target_desktop)
         _get_apps(selected_desktop)
 
