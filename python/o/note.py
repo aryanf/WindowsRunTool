98f82e6249afdce6271c2872df93b9c61b84b2bd
aryan.firouzian <aryan.firouzian@hansencx.com>
2024-07-06 15:14:06 +0300

    Add new subcommand that open a new instance of notepad++

--- python/o/note.py
+++ python/o/note.py
@@ -27,3 +27,17 @@ def dir(message: SubCommandMessage):
 Open the directory of the files
 '''
     subprocess.Popen(['explorer', document_path], shell=True)
+
+def new(message: SubCommandMessage):
+    '''
+Open notepad++ as new instance if not parameter is provided, or 
+create a file in Document directory with foldable structure
+{switch_1}: file name which is stored in default location
+'''
+    if(message.switch_1==''):
+        subprocess.Popen(['start', app_path, '-multiInst'], shell=True)
+    else:
+        note_path = os.path.join(document_path, f'{message.switch_1}.diff')
+        if not os.path.exists(note_path):
+            shutil.copy2(template_path, note_path) 
+        subprocess.Popen(['start', app_path, '-ldiff', note_path], shell=True)
