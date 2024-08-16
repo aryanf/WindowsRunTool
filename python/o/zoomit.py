0153f1686cd74f42c097959a438399202c796abf
aryan.firouzian <aryan.firouzian@hansencx.com>
2024-08-17 02:04:44 +0300

    Add zoomit

---
+++ python/o/zoomit.py
@@ -0,0 +1,15 @@
+from message import (MainCommandMessage, SubCommandMessage, get_open_source_app_dir)
+import subprocess
+import os
+import time
+
+app_path = os.path.join(get_open_source_app_dir(), 'ZoomIt', 'ZoomIt.exe')
+
+def main(message: MainCommandMessage):
+    '''
+Open ZoomIt
+'''
+    print('zoomit')
+    print('use Ctrl+1 to zoom in, Ctrl+2 to draw.')
+    time.sleep(2)
+    subprocess.Popen([app_path])
